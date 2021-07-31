e334a195cae2ea4856c208bde7e3b8ee4ca69813
Qardruss <78308435+Qardruss@users.noreply.github.com>
2021-07-31 11:28:08 +0100

    Add files via upload

---
+++ 1.17/Dockerfile
@@ -0,0 +1,6 @@
+FROM ubuntu
+WORKDIR /server
+COPY initialize.sh init.sh
+COPY run.sh runServer.sh
+COPY Dockerfile.txt ThisIsMe
+EXPOSE 25565
---
+++ 1.17/initialize.sh
@@ -0,0 +1,11 @@
+apt update
+apt install -y wget
+#apt install -y default-jre
+unminimalize
+wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
+java -version
+
+#add-apt-repository ppa:uprising/java
+
+#apt update
+#apt install -y oracle-java16-installer --install-recommends
